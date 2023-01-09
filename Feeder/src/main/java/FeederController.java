63004281f7a84539ced7f3ad1d08ab741c397616
marianabordes <mariana.bordes101@alu.ulpgc.es>
2023-01-09 12:17:23 +0000

    TimerTask

--- Feeder/src/main/java/FeederController.java
+++ Feeder/src/main/java/FeederController.java
@@ -1,17 +1,27 @@
 import java.io.IOException;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class FeederController {
-    public static void main(String[] args) throws IOException {
-        controller();
-    }
-    public static void controller() throws IOException {
-        DataManager dataManager = new DataManager();
+    public void controller() throws IOException {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                DataManager dataManager = new DataManager();
+                String datos = null;
 
-        String datos = DataExtractor.getDataUrls();
-        String json = DataExtractor.getData(datos);
-        List<Weather> weathers = dataManager.filterData(json);
-        dataManager.storeData(weathers);
-
+                try {
+                    datos = DataExtractor.getDataUrls();
+                    String json = DataExtractor.getData(datos);
+                    List<Weather> weathers = dataManager.filterData(json);
+                    dataManager.storeData(weathers);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        timer.scheduleAtFixedRate(task, 0, 3600000);
     }
 }
